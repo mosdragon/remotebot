621dfd5e562f64f61a20cbec6b12faa976aaaf34
Osama Sakhi <msakhi21@gmail.com>
2016-05-20 01:35:01 +0000

    Complete with simple web interface

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
 import serial
 import time
-
+import flask
+from flask import render_template
 
 baudrate = 9600
 comPort = "/dev/ttyACM0"
+ser = None
+app = flask.Flask("Brobot")
+
+
+@app.route("/forward", methods=["POST"])
+def forward():
+    ser.write("F")
+    return render_template("index.html")
+
+
+@app.route("/reverse", methods=["POST"])
+def reverse():
+    ser.write("B")
+    return render_template("index.html")
+
+@app.route("/left", methods=["POST"])
+def left():
+    ser.write("L")
+    return render_template("index.html")
+
+@app.route("/right", methods=["POST"])
+def right():
+    ser.write("R")
+    return render_template("index.html")
+
+@app.route("/stop", methods=["POST"])
+def stop():
+    ser.write("S")
+    return render_template("index.html")
+    
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
     print "Hello"
     ser = serial.Serial(comPort, baudrate)
-    ser.write("F")
-    time.sleep(2)
-    ser.write("S")
-
+    app.run(debug=True)
